2d2a28ccdfd03d017e77eb5a4d06c9b88fda311c
Olaf Rosendahl <olafrosendahl@gmail.com>
2022-09-18 10:41:03 +0200

    fix: match update bug

--- pages/api/matches/[id]/index.tsx
+++ pages/api/matches/[id]/index.tsx
@@ -1,5 +1,6 @@
 import { Result } from '@prisma/client';
 import HttpStatusCode from 'http-status-typed';
+import { prisma } from 'lib/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
